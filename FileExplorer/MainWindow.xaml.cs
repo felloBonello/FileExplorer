17fda6d214213f81181b49d7f5f47814d6e847d3
Bonello <j_bonello2@fanshawec.ca>
2017-08-30 12:29:02 -0400

    Initial commit, review needed

---
+++ FileExplorer/MainWindow.xaml.cs
@@ -0,0 +1,209 @@
+﻿
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Runtime.Remoting.Messaging;
+
+namespace FileExplorer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private delegate Node ParseDirDelegate();
+
+        //tree display source
+        ObservableCollection<Node> treeCtx = new ObservableCollection<Node>();
+        Node firstNode;
+
+        //Dependancy properties for all labels
+        public string parseDir
+        {
+            get { return (string)GetValue(parseDirProp); }
+            set { SetValue(parseDirProp, value); }
+        }
+
+        public static readonly DependencyProperty parseDirProp =
+            DependencyProperty.Register("parseDir", typeof(string), typeof(MainWindow), new PropertyMetadata(""));
+
+        public int folders
+        {
+            get { return (int)GetValue(foldersProp); }
+            set { SetValue(foldersProp, value); }
+        }
+
+        public static readonly DependencyProperty foldersProp =
+            DependencyProperty.Register("folders", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+        public int files
+        {
+            get { return (int)GetValue(filesProp); }
+            set { SetValue(filesProp, value); }
+        }
+
+        public static readonly DependencyProperty filesProp =
+            DependencyProperty.Register("files", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+        public int selectedFolders
+        {
+            get { return (int)GetValue(selectedFoldersProp); }
+            set { SetValue(selectedFoldersProp, value); }
+        }
+
+        public static readonly DependencyProperty selectedFoldersProp =
+            DependencyProperty.Register("selectedFolders", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+        public int selectedFiles
+        {
+            get { return (int)GetValue(selectedFilesProp); }
+            set { SetValue(selectedFilesProp, value); }
+        }
+
+        public static readonly DependencyProperty selectedFilesProp =
+            DependencyProperty.Register("selectedFiles", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+        public long sizeInBytes
+        {
+            get { return (long)GetValue(sizeInBytesProp); }
+            set { SetValue(sizeInBytesProp, value); }
+        }
+
+        public static readonly DependencyProperty sizeInBytesProp =
+            DependencyProperty.Register("sizeInBytes", typeof(long), typeof(MainWindow), new PropertyMetadata((long)0));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //load up last file used
+            LoadPathFile();
+            DataContext = this;           
+        }
+
+        public void LoadPathFile()
+        {
+            //if saved path exists load else parse current dir
+            if (File.Exists("./path.txt"))
+                try
+                {
+                    using (StreamReader inputFile = new StreamReader("path.txt", true))
+                    {
+                        parseDir = inputFile.ReadLine();
+                    }
+                }
+                catch(Exception err)
+                {
+                    System.Windows.MessageBox.Show("There has been an error loading the previously parsed directory.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            else
+            {
+                //Get full path of current directory
+                parseDir = Path.GetFullPath(".");
+            }               
+        }
+
+        private void createFirstNode()
+        {
+            //initialize first node to hold all other nodes
+            DirectoryInfo dirInfo = new DirectoryInfo(parseDir);
+            firstNode = new Node()
+            {
+                name = dirInfo.Name,
+                fullPath = parseDir,
+                byteSize = 0,
+                parent = null,
+                iconLoc = Node.folderIcon,
+                isFile = false,
+            };
+            ++Node.folders;
+            //add first node to display         
+            treeCtx = new ObservableCollection<Node>()
+            {
+                firstNode
+            };         
+        }
+
+        //update all dependacy properties to current static values in Node class
+        private void UpdateCounts()
+        {
+            files = Node.files;
+            folders = Node.folders;
+            selectedFolders = Node.selectedFolders;
+            selectedFiles = Node.selectedFiles;
+            sizeInBytes = Node.selectedBytes;
+        }
+
+        private void ParseNewDir()
+        {
+            //resets all counts and displays to 0
+            Node.resetCounts();
+            UpdateCounts();
+
+            //create first node, empty tree display, display parsing msg
+            createFirstNode();
+            fileDisplay.ItemsSource = null;
+            parseMsg.Visibility = Visibility.Visible;
+
+            //recursively parse directory asynchronously 
+            ParseDirDelegate parseDelegate = new ParseDirDelegate(firstNode.DirParse);
+            parseDelegate.BeginInvoke(theCallback, this);
+        }
+
+        public void theCallback(IAsyncResult theResults)
+        {
+            AsyncResult result = (AsyncResult)theResults;
+            ParseDirDelegate parseDelegate = (ParseDirDelegate)result.AsyncDelegate;
+            Node node = parseDelegate.EndInvoke(theResults);
+            //Back to the GUI thread to update tree display and counts with newly parsed directory
+            //hide parsing msg and display complete msg
+            this.Dispatcher.Invoke(DispatcherPriority.Background, ((Action)(() => {
+                parseMsg.Visibility = Visibility.Hidden;
+                firstNode = node;
+                firstNode.isChecked = false;
+                UpdateCounts();
+                fileDisplay.ItemsSource = treeCtx;
+                System.Windows.MessageBox.Show(Properties.Resources.parseSuccess);
+            })));
+        }
+
+        //directory changed -> parse the new directory
+        private void dirDisplay_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ParseNewDir();
+        }
+
+        //checkbox clicked update counts display
+        private void chk_clicked(object sender, RoutedEventArgs e)
+        {
+            UpdateCounts();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //display folder dialog so user can select directory to parse
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    parseDir = fbd.SelectedPath;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //save current directory to text file on window close
+            try
+            {
+                System.IO.File.WriteAllText(@"path.txt", parseDir);
+            }
+            catch(Exception err)
+            {
+                System.Windows.MessageBox.Show("There has been an error saving the current directory.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ FileExplorer/Node.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace FileExplorer
+{
+    public class Node : INotifyPropertyChanged
+    {
+        public Node()
+        {
+            children = new ObservableCollection<Node>();
+            isChecked = false;
+        }
+
+        //static vars to hold current counts
+        public static int files = 0;
+        public static int folders = 0;
+        public static int selectedFolders = 0;
+        public static int selectedFiles = 0;
+        public static long selectedBytes = 0;
+
+        //static vars to hold relative file paths for icons
+        public static Uri fileIcon = new Uri("..\\file.png", UriKind.Relative);
+        public static Uri folderIcon = new Uri("..\\folder.png", UriKind.Relative);
+        public static Uri driveIcon = new Uri("..\\drive.png", UriKind.Relative);
+        public static Uri loadingIcon = new Uri("..\\loading.png", UriKind.Relative);
+        public static Uri pathFile = new Uri("file.txt", UriKind.Relative);
+
+        //class members
+        public ObservableCollection<Node> children { get; set; }
+        public Node parent { get; set; }
+        public long byteSize { get; set; }
+        public string name { get; set; }
+        public Uri iconLoc { get; set; }
+        public string fullPath { get; set; }
+        public bool isFile { get; set; }
+        private bool? prevVal { get; set; }
+
+        //stores whether current node has been entered already during current recursive call
+        private bool reentrancyCheck = false;  
+
+        //binding for checkbox of current node
+        private bool? _isChecked { get; set; }  
+        public bool? isChecked {
+            get
+            {
+                return _isChecked;
+            }
+            set
+            {
+                if(isChecked != value)
+                {
+                    //if node has been etnered already than exit
+                    if (reentrancyCheck)
+                        return;
+                    //note that node has been entered
+                    reentrancyCheck = true;
+                    //store previous value
+                    prevVal = _isChecked;
+                    _isChecked = value;
+                    UpdateCheckState();
+                    //notify prop changed
+                    OnPropertyChanged("IsChecked");
+                    //reset entrancy for next recursive call
+                    reentrancyCheck = false;                   
+                }
+            }
+        }
+
+        private void updateCounts()
+        {
+            //decide operation ie. increase or decrease
+            string op = "";
+            if(prevVal != null) //if prev vall is null do nothing
+            {
+                if (isChecked == true) //increase
+                    op = "+";
+                else if (isChecked == false || (isChecked == null && prevVal == true)) //decrease
+                    op = "-";
+            }
+                
+            if (op == "+")
+            {
+                //increase file or folder count
+                if (isFile)
+                    ++selectedFiles;
+                else
+                    ++selectedFolders;
+                //increase byte count (folders are 0)
+                selectedBytes += byteSize;
+            }
+            else if (op == "-")
+            {
+                //decrease file or folder count
+                if (isFile)
+                    --selectedFiles;
+                else
+                    --selectedFolders;
+                //decrease byte count (folders are 0)
+                selectedBytes -= byteSize;
+            }
+        }
+
+        private void UpdateCheckState()
+        {
+            updateCounts();
+
+            //update children
+            if (children.Count != 0)
+                UpdateChildrenCheckState();
+            //update parent
+            if(parent != null)
+            {
+                bool? parentIsChecked = parent.DetermineCheckState();
+                parent.isChecked = parentIsChecked;
+            }
+        }
+
+        private void UpdateChildrenCheckState()
+        {
+            //recursively update children by calling IsChecked {set;}
+            foreach (var c in children)
+                if (isChecked != null)
+                    c.isChecked = isChecked;
+        }
+
+        //determine proper state of a parent node
+        private bool? DetermineCheckState()
+        {
+            //if all children checked parent is checked
+            bool allChildrenChecked = children.Count(c => c.isChecked == true) == children.Count;
+            if (allChildrenChecked)
+                return true;
+
+            //if all children unchecked parent is unchecked
+            bool allChildrenUnchecked = children.Count(c => c.isChecked == false) == children.Count;
+            if (allChildrenUnchecked)
+                return false;
+
+            return null;
+        }
+
+        //reset all counts to 0
+        public static void resetCounts()
+        {
+            files = 0;
+            folders = 0;
+            selectedFiles = 0;
+            selectedFolders = 0;
+            selectedBytes = 0;
+        }
+
+        //recurively parse a Node which contains the directory at hand
+        public Node DirParse()
+        {
+            try
+            {
+                //add each child directory to children, increase folder count
+                foreach (string d in Directory.GetDirectories(this.fullPath))
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(d);
+                    Node curDNode = new Node()
+                    {
+                        parent = this,
+                        name = dirInfo.Name,
+                        fullPath = dirInfo.FullName,
+                        byteSize = 0,
+                        iconLoc = folderIcon,
+                        isFile = false
+                    };
+
+                    this.children.Add(curDNode);
+                    ++folders;
+                    //recursive call for each directory found within current dir
+                    curDNode.DirParse();
+                }
+
+                //add each child file to children, increase file count
+                foreach (string f in Directory.GetFiles(this.fullPath))
+                {
+                    FileInfo fileInfo = new FileInfo(f);
+                    Node curFNode = new Node()
+                    {
+                        parent = this,
+                        name = fileInfo.Name,
+                        fullPath = fileInfo.FullName,
+                        byteSize = fileInfo.Length,
+                        iconLoc = fileIcon,
+                        isFile = true
+                    };
+                    this.children.Add(curFNode);
+                    ++files;
+                }
+
+                return this;
+            }
+            catch(Exception err)
+            {
+                System.Windows.MessageBox.Show("There has been an error parsing the selected directory.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            return null;
+        }
+
+        void OnPropertyChanged(string prop)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
